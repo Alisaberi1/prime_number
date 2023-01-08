15115b700b30d62d723c329a6aa764db49df8efc
Ali $@ <90723218+Alisaberi1@users.noreply.github.com>
2023-01-08 06:56:23 +0330

    Update today.py

--- today.py
+++ today.py
@@ -1,28 +1,6 @@
 
-# without function 
-"""
-a=int(input())
-b=int(input())
-if a==1:
-    for number in range (a+1, b+ 1):
-        for i in range (2, number):
-            if (number % i) == 0:
-                break
-        else:
-            print(number)
-if a!=1:
-    for number in range (a, b+ 1):
-        for i in range (2, number):
-            if (number % i) == 0:
-                break
-        else:
-            # cause of error is if a==1 it will consider 1 as a prime number :) change it 
-            #if a==1:
-                #print(number-[0]
-            #else:
-            print(number)
-"""
-# with function
+
+
 def is_prime(num):
     if num == 1:return 0
     for i in range(2, num):
